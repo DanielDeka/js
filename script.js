a84d01c1169ac59e31f8c94991debae3632e416b
DanielDeka <dekaslr8@gmail.com>
2018-03-05 21:33:31 -0900

    fixxx

--- script.js
+++ script.js
@@ -5,20 +5,19 @@ function processForm() {
     var result;
 
     for (var i=0; i < document.tempForm.choice.length; i++) {
-   
+        
         if (document.tempForm.choice[i].checked) {
             tempType = document.tempForm.choice[i].value;
         }
     }
 
-    /*conditional to perform calculations based on choice */
-
-    if (tempType === 'fahrenheit') {
+    if (tempType == 'fahrenheit') {
         result = temperature * 9/5 + 32;
     }
+    
     else {
-     result = (temperature -  32)  *  5/9;
+        result = (temperature -  32)  *  5/9;
     }
-
-    document.tempForm.resultField.value = result;
+    
+   document.tempForm.resultField.value = result;
 }
